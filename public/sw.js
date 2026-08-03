edd2cb4ab1455d5260d2d35d8252886021f188d3
Drmacze DEV <drma801@gmail.com>
2026-08-03 18:48:52 +0700

    Refresh PWA cache for iPhone layout fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'cybertrmx-v5';
-const ASSETS = ['./', './index.html', './styles.css', './intel.css', './app.js', './intel.js', './guide.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'cybertrmx-v6';
+const ASSETS = ['./', './index.html', './styles.css', './intel.css', './mobile.css', './app.js', './intel.js', './guide.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {

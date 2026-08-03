292b575395ac313f44e887d9143c4981f7ef0e4e
Drmacze DEV <drma801@gmail.com>
2026-08-04 05:19:58 +0700

    Add CYBERTRMX backend runtime configuration

---
+++ public/backend-config.js
@@ -0,0 +1,7 @@
+window.CYBERTRMX_BACKEND = Object.freeze({
+  url: 'https://ydaeukhqwishlrjyfktk.supabase.co',
+  publishableKey: 'sb_publishable_XNXU6SVeM-D477Ymy1ORsw_4hCHOll9',
+  operationsFunction: 'cybertrmx-ops',
+  checkinFunction: 'cybertrmx-checkin',
+  locationsFunction: 'cybertrmx-locations'
+});

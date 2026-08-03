8a88be071578216be5f7774b2eef5b81bab42b8d
Drmacze DEV <drma801@gmail.com>
2026-08-03 20:04:09 +0700

    Refresh PWA cache after Guide fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cybertrmx-v10';
+const CACHE = 'cybertrmx-v11';
 const ASSETS = ['./', './index.html', './styles.css', './intel.css', './mobile.css', './refine.css', './landing.css', './profile.css', './app.js', './intel.js', './guide.js', './profile.js', './motion.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));

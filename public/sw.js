155930e8e72d024af97c20a39d853ce253c2d185
Drmacze DEV <drma801@gmail.com>
2026-08-03 19:15:00 +0700

    Refresh PWA cache for fullscreen navigation redesign

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cybertrmx-v8';
+const CACHE = 'cybertrmx-v9';
 const ASSETS = ['./', './index.html', './styles.css', './intel.css', './mobile.css', './refine.css', './landing.css', './app.js', './intel.js', './guide.js', './motion.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));

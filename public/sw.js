9aee432da535e5dc067ff9511b446f217f32c574
Drmacze DEV <drma801@gmail.com>
2026-08-03 19:56:49 +0700

    Cache redesigned profile assets

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'cybertrmx-v9';
-const ASSETS = ['./', './index.html', './styles.css', './intel.css', './mobile.css', './refine.css', './landing.css', './app.js', './intel.js', './guide.js', './motion.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'cybertrmx-v10';
+const ASSETS = ['./', './index.html', './styles.css', './intel.css', './mobile.css', './refine.css', './landing.css', './profile.css', './app.js', './intel.js', './guide.js', './profile.js', './motion.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {

6bdbafd88e43dbd6316dc2bf29dc594cdde58c02
Drmacze DEV <drma801@gmail.com>
2026-08-05 22:56:00 +0700

    Expose persistent Job API in staging configuration

--- public/backend-config.js
+++ public/backend-config.js
@@ -2,6 +2,7 @@ window.CYBERTRMX_BACKEND = Object.freeze({
   url: 'https://ydaeukhqwishlrjyfktk.supabase.co',
   publishableKey: 'sb_publishable_XNXU6SVeM-D477Ymy1ORsw_4hCHOll9',
   operationsFunction: 'cybertrmx-ops',
+  jobsFunction: 'cybertrmx-jobs',
   checkinFunction: 'cybertrmx-checkin',
   locationsFunction: 'cybertrmx-locations'
 });
